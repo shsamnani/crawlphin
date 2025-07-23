2a0c3346483fe7985085472144aafebf48a7ef20
Shehzad Samnani <shehzadhsamnani@gmail.com>
2025-07-23 16:51:54 +0500

    Defines mongoose schema for MongoDB

---
+++ src/models/UrlAnalysis.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const UrlAnalysisSchema = new mongoose.Schema({
+  url: { type: String, required: true, unique: true },
+  status: {
+    type: String,
+    enum: ["queued", "running", "done", "error"],
+    default: "queued",
+  },
+  htmlVersion: String,
+  pageTitle: String,
+  headingCounts: {
+    h1: { type: Number, default: 0 },
+    h2: { type: Number, default: 0 },
+    h3: { type: Number, default: 0 },
+    h4: { type: Number, default: 0 },
+    h5: { type: Number, default: 0 },
+    h6: { type: Number, default: 0 },
+  },
+  internalLinks: { type: Number, default: 0 },
+  externalLinks: { type: Number, default: 0 },
+  brokenLinks: [
+    {
+      link: String,
+      statusCode: Number,
+    },
+  ],
+  hasLoginForm: { type: Boolean, default: false },
+  lastAnalyzed: { type: Date, default: Date.now },
+  errorMessage: String, // To store details if status is 'error'
+});
+
+module.exports = mongoose.model("UrlAnalysis", UrlAnalysisSchema);
